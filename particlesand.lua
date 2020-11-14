3a602872667b9cb20bb76e574b92a66bf24192ae
minecart <58528828+Pierre-Angelo@users.noreply.github.com>
2020-11-14 09:12:26 +0400

    ajusted particles

--- particlesand.lua
+++ particlesand.lua
@@ -1,50 +1,49 @@
 local function create_particule(px,py,pangle)
-    local particle = {}
-    particle.x = px
-    particle.y = py
-    particle.angle = pangle
-    particle.speed = math.random(100,200)
-    particle.vx = math.cos(particle.angle) * particle.speed
-    particle.vy =  math.sin(particle.angle) * particle.speed
-    particle.alpha = 1
-    particle.valpha = math.random(50,60)/10    
-    particle.color = sable
+    local particule = {}
+    particule.x = px
+    particule.y = py
+    particule.angle = pangle
+    particule.speed = math.random(100,200)
+    particule.vx = math.cos(particule.angle) * particule.speed
+    particule.vy =  math.sin(particule.angle) * particule.speed
+    particule.alpha = 1
+    particule.valpha = math.random(50,60)/10    
+    particule.color = sable
 
     local action = {
-      [1] = function () particle.x = particle.x + 15 particle.y = particle.y - 15 end,
-      [2] = function ()  particle.x = particle.x + 15 particle.y = particle.y - 15 end,
-      [3] = function ()  particle.x = particle.x - 15 particle.y = particle.y + 15 end,
-      [4] = function ()  particle.x = particle.x - 15 particle.y = particle.y + 15 end,
+      [1] = function () particule.x = particule.x + 10 + math.random(-10,10) particule.y = particule.y - 10 end,
+      [2] = function ()  particule.x = particule.x + 10 + math.random(-10,10)particule.y = particule.y + 10 end,
+      [3] = function ()  particule.x = particule.x - 10 + math.random(-10,10) particule.y = particule.y + 10 end,
+      [4] = function ()  particule.x = particule.x - 10 + math.random(-10,10) particule.y = particule.y - 1105 end,
   }
-      
-      action[math.random(4)]()
+    action[math.random(4)]()
     
-    function particle.update(dt)
-      particle.x = particle.x + particle.vx * dt
-      particle.y = particle.y + particle.vy * dt
-      particle.alpha = particle.alpha - particle.valpha *dt
+    function particule.update(dt)
+      particule.x = particule.x + particule.vx * dt
+      particule.y = particule.y + particule.vy * dt
+      particule.alpha = particule.alpha - particule.valpha *dt
     end
     
-    function particle.draw()
-      love.graphics.setColor(particle.color,particle.alpha)
-      love.graphics.circle("fill",particle.x,particle.y,2)
+    function particule.draw()
+      love.graphics.setColor(particule.color,particule.alpha)
+      love.graphics.circle("fill",particule.x,particule.y,2)
       
  
     end
-    return particle
+    return particule
   end
   
-  function create_emitter(px,py,pnbparticle)
+  function create_emitter(px,py,pnbparticule)
     local emitter = {}
     emitter.x = px
     emitter.y = py
-    emitter.N = pnbparticle
+    emitter.N = pnbparticule
     emitter.list = {}
     
     function emitter.init()
       for i=1 , emitter.N do
-        local newparticle= create_particule(emitter.x,emitter.y,math.pi*0.5 + math.random(-10,10)/15)
-        table.insert(emitter.list,newparticle)
+        local newparticule= create_particule(emitter.x,emitter.y,math.pi*0.5 + math.random(-10,10)/15)
+        table.insert(emitter.list,newparticule)
       end
     end
     
@@ -56,10 +55,10 @@ local function create_particule(px,py,pangle)
           particule.alpha = math.random()
           local angle = math.random() * math.pi*2
           local action = {
-            [1] = function () particule.x = emitter.x + 15 + math.random(-10,10) particule.y = emitter.y - 15 end,
-            [2] = function ()  particule.x = emitter.x + 15 + math.random(-10,10)particule.y = emitter.y + 15 end,
-            [3] = function ()  particule.x = emitter.x - 15 + math.random(-10,10) particule.y = emitter.y + 15 end,
-            [4] = function ()  particule.x = emitter.x - 15 + math.random(-10,10) particule.y = emitter.y - 15 end,
+            [1] = function () particule.x = emitter.x + 10 + math.random(-10,10) particule.y = emitter.y - 10 end,
+            [2] = function ()  particule.x = emitter.x + 10 + math.random(-10,10)particule.y = emitter.y + 10 end,
+            [3] = function ()  particule.x = emitter.x - 10 + math.random(-10,10) particule.y = emitter.y + 10 end,
+            [4] = function ()  particule.x = emitter.x - 10 + math.random(-10,10) particule.y = emitter.y - 1105 end,
         }
           action[math.random(4)]()
         end
@@ -67,8 +66,8 @@ local function create_particule(px,py,pangle)
     end
   
     function emitter.draw()
-      for _,particle in pairs(emitter.list) do
-        particle.draw()
+      for _,particule in pairs(emitter.list) do
+        particule.draw()
       end
     end
   
