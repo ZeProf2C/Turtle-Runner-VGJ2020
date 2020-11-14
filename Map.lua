24552ec783f25546575ebdfd48d486c040db822d
minecart <58528828+Pierre-Angelo@users.noreply.github.com>
2020-11-14 12:39:38 +0400

    End working

--- Map.lua
+++ Map.lua
@@ -27,19 +27,31 @@ function Map.update(dt, scrollSpeed)
     
 end
 
+
 function Map.draw()
-    if Map.oy <= -Map.image.height then
-        if Map.isEnd then
-            Map.oy = -1600
-        else
-            Map.oy = 0
+    
+    if Map.isEnd then
+        if Map.oy <= -Map.image.height*2  then
+            Map.oy = -Map.image.height*2
+            
         end
-    end 
+    elseif Map.oy <= -Map.image.height   then      
+        Map.oy = 0
+    end
+ 
     love.graphics.setColor(1, 1, 1)
     love.graphics.draw(Map.image.src, 0, 0, 0, 1, 1, 0, Map.oy)
     if Map.isEnd then
-        love.graphics.draw(Map.image.endScreen, 0, 0, 0, 1, 1, 0, Map.oy+Map.image.height)
+        love.graphics.draw(Map.image.src, 0, 0, 0, 1, 1, 0, Map.oy+Map.image.height)
+        love.graphics.draw(Map.image.endScreen, 0, 0, 0, 1, 1, 0, Map.oy+Map.image.height*2)
+    
+        
     else
         love.graphics.draw(Map.image.src, 0, 0, 0, 1, 1, 0, Map.oy+Map.image.height)
+        
     end
+    if  Map.isEnd then
+
+    end
+    
 end
